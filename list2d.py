ac7478d2d50f6417c6024a9036a4543f5a7a6d0a
Glenn Marcellino Oktaviyanto <24_gleenmarchellino@student.smkti.net>
2025-07-21 14:42:33 +0800

    menambahkan list2d.py

---
+++ list2d.py
@@ -0,0 +1,9 @@
+Nilai = [[10, 20, 30],
+         [40, 50, 60],
+         [70, 80, 90]]
+sum = 0
+for i in range (len(Nilai)):
+    for j in range (len(Nilai[i])):
+        sum = sum + Nilai[i][j]
+    print(sum)
+    sum = sum * 0
